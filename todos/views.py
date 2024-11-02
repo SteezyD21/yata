72f7ea608dc88c8a654e87f80a6361d678f89749
SteezyD21 <70117457+SteezyD21@users.noreply.github.com>
2024-11-02 14:52:27 +0000

    Getting started with C: add a title mode

--- todos/views.py
+++ todos/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import forms
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("Hello world")
+
+    form = forms.CreateTodoForm() 
+    context = {"form": form}      
+    return render(request, "todos/index.html", context)
