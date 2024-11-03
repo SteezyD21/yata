2f372435847b9dfe8536ff5704f512505e59ce97
Douglas Aphane <douglas.aphane@umuzi.org>
2024-11-03 10:29:32 +0200

    update

--- todos/static/todos/utils.js
+++ todos/static/todos/utils.js
@@ -1,1 +1,26 @@
-alert("Hello static files")
+function showOrHideEmptyMessage() {
+    
+    const itemClass = "todo_item" 
+    const emptyListId = "todo_items_empty"
+
+    const itemCount = Array.from(document.getElementsByClassName(itemClass)).length;
+
+    if (itemCount === 0) {
+        document.getElementById(emptyListId).style.display = "block";
+    } else {
+        document.getElementById(emptyListId).style.display = "none";
+    }
+}
+
+window.onload = function () {
+    // note the ID here. It is the target from our form's htmx post
+    const targetNode = document.getElementById("todo_items");
+
+    const config = {
+        childList: true,
+    };
+
+    const observer = new MutationObserver(showOrHideEmptyMessage);
+
+    observer.observe(targetNode, config);
+};
