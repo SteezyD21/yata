d934faf7a37de0f5a576ba0ae24d02bf6bb9f209
SteezyD21 <70117457+SteezyD21@users.noreply.github.com>
2024-11-03 00:09:10 +0000

    add todos as an installed app

--- yata/settings.py
+++ yata/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     "crispy_forms",
     "crispy_tailwind",
+    "todos",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
